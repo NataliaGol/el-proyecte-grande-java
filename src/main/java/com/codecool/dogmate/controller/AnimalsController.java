c23ee4864ef7f72be77b185e2555029862a15587
Natalia S <nataliahreast@gmail.com>
2023-02-19 14:38:03 +0100

    Implement adding new dog, and getting all dogs

--- src/main/java/com/codecool/dogmate/controller/AnimalsController.java
+++ src/main/java/com/codecool/dogmate/controller/AnimalsController.java
@@ -1,16 +1,32 @@
 package com.codecool.dogmate.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.codecool.dogmate.dto.AnimalDto;
+import com.codecool.dogmate.service.AnimalService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/animals")
 public class AnimalsController {
 
-    @GetMapping
-    public String AnimalsPage() {
+    private final AnimalService animalService;
 
-        return "animals";
+    public AnimalsController(AnimalService animalService) {
+        this.animalService = animalService;
     }
+
+    @GetMapping
+    List<AnimalDto> getAllDogs() {
+        return animalService.findAllDogs();
+    }
+
+    @PostMapping
+    ResponseEntity<HttpStatus> addDog(@RequestBody AnimalDto dto) {
+        animalService.addDog(dto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }
