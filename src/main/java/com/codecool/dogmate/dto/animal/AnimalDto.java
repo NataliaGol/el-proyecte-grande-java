04b7da6a7498ed12336504f8ce253bd6da080982
Natalia S <nataliahreast@gmail.com>
2023-03-13 22:06:14 +0100

    Fix application not compiling because of AnimalDto missing + fix query in province repository

---
+++ src/main/java/com/codecool/dogmate/dto/animal/AnimalDto.java
@@ -0,0 +1,18 @@
+package com.codecool.dogmate.dto.animal;
+
+import lombok.Data;
+
+@Data
+public class AnimalDto {
+
+    private Integer id;
+    private String name;
+    private Integer animalTypeId;
+    private Integer breedId;
+    private Integer userId;
+    private Integer birthYear;
+    private String pictureLocation;
+    private String description;
+    private Boolean isMale;
+
+}
--- src/main/java/com/codecool/dogmate/repository/ProvincesRepository.java
+++ src/main/java/com/codecool/dogmate/repository/ProvincesRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface ProvincesRepository extends JpaRepository<Province, Integer> {
 
-    @Query("SELECT DISTINCT d FROM Province d LEFT JOIN FETCH d.voivodeship.name")
+    @Query("SELECT DISTINCT d FROM Province d LEFT JOIN FETCH d.voivodeship")
     List<Province> findAllBy();
 }
