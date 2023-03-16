b92a12d969c19c49a362be942677fa5c7f423076
Natalia S <nataliahreast@gmail.com>
2023-03-16 23:40:32 +0100

    Call backend from frontend for registration

--- my-app/src/components/SignUp.js
+++ my-app/src/components/SignUp.js
@@ -1,17 +1,28 @@
-import { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import {useEffect, useState} from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
 import 'animate.css';
 import login from '../assets/video/login.mp4'
 
-import { Link } from "react-router-dom";
 export const SignUp = () => {
+    const navigate = useNavigate();
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const [index, setIndex] = useState(1);
-    const toRotate = [ "przy", "psyjaciela" ];
+    const toRotate = ["przy", "psyjaciela"];
     const period = 1000;
+
+    const [registrationMessage, setRegistrationMessage] = useState("");
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    });
+    const {name, email, password, passwordConfirmation} = form;
 
     useEffect(() => {
         let ticker = setInterval(() => {
@@ -46,46 +57,106 @@ export const SignUp = () => {
         }
     }
 
+    function handleChange(e) {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        axios
+            .post(
+                "http://localhost:9090/users",
+                {
+                    name: name,
+                    email: email,
+                    password: password,
+                },
+                {withCredentials: true}
+            )
+            .then((response) => {
+                console.log("registration res", response);
+            })
+            .catch((error) => {
+                console.log("registration error", error);
+                setRegistrationMessage("registration error");
+            });
+        setRegistrationMessage("registered successfully");
+        navigate("/login");
+        setForm({
+            name: "",
+            email: "",
+            password: "",
+            passwordConfirmation: "",
+            registrationMessage: "",
+        });
+    }
+
     return (
         <section className="signup" id="signup">
             <Container>
                 <div className='signup'>
-                    <video src={login} autoPlay loop muted /> </div>
+                    <video src={login} autoPlay loop muted/>
+                </div>
                 <Row className="aligh-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <div className="Auth-form-content">
-                        <h3 className="Auth-form-title">ZAŁÓŻ KONTO</h3>
-                        <div className="form-group mt-3">
-                            <input
-                                type="email"
-                                className="form-control mt-1"
-                                placeholder="twoje imię i nazwisko"
-                            />
-                            <p><label>IMIĘ I NAZWISKO</label></p>
-                        </div>
-                        <div className="form-group mt-3">
-                            <input
-                                type="email"
-                                className="form-control mt-1"
-                                placeholder="twój mail"
-                            />
-                            <p><label>E-MAIL</label></p>
-                        </div>
-                        <div className="form-group mt-3">
-                            <input
-                                type="password"
-                                className="form-control mt-1"
-                                placeholder="twoje hasło"
-                            />
-                            <p><label>HASŁO</label></p>
-                        </div>
-                        <div className="d-grid gap-2 mt-3">
-                            <button type="submit" className="btn btn-primary">
-                                <Link to="/signup">ZAPISZ</Link>
-                            </button>
-                        </div>
+                        <form className="Auth-form-content" onSubmit={handleSubmit}>
+                            <h3 className="Auth-form-title">ZAŁÓŻ KONTO</h3>
+                            <div className="form-group mt-3">
+                                <input
+                                    type="text"
+                                    className="form-control mt-1"
+                                    placeholder="twoje imię i nazwisko"
+                                    name="name"
+                                    value={name}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <p><label>IMIĘ I NAZWISKO</label></p>
+                            </div>
+                            <div className="form-group mt-3">
+                                <input
+                                    type="email"
+                                    className="form-control mt-1"
+                                    placeholder="twój mail"
+                                    name="email"
+                                    value={email}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <p><label>E-MAIL</label></p>
+                            </div>
+                            <div className="form-group mt-3">
+                                <input
+                                    type="password"
+                                    className="form-control mt-1"
+                                    placeholder="twoje hasło"
+                                    name="password"
+                                    value={password}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <p><label>HASŁO</label></p>
+                            </div>
+                            <div className="form-group mt-3">
+                                <input
+                                    type="password"
+                                    className="form-control mt-1"
+                                    placeholder="potwierdź hasło"
+                                    name="passwordConfirmation"
+                                    value={passwordConfirmation}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <p><label>POTWIERDŹ HASŁO</label></p>
+                            </div>
+                            <div className="d-grid gap-2 mt-3">
+                                <button type="submit" className="btn btn-primary">
+                                    ZAREJESTRUJ
+                                </button>
+                            </div>
 
-                        </div>
+                        </form>
+                        <div><h4>{registrationMessage}</h4></div>
                         <h2><Link to="/login">MASZ KONTO? ZALOGUJ SIĘ</Link></h2>
                     </Col>
                 </Row>
